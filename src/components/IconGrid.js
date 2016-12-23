438d503e64f02af80ed678185ec2b348060ddaf1
bentatum <bentatum@me.com>
2016-12-23 18:18:24 -0500

    master(#): lazy-loading icon sets.

--- src/components/IconGrid.js
+++ src/components/IconGrid.js
@@ -1,10 +1,14 @@
 
+import $ from 'jquery'
 import React from 'react'
+import { chunk } from 'lodash'
 import util from '../theme/utility'
 import { connect } from 'react-redux'
 import { Container, IconCard } from '.'
-import { scale, breakpoints } from '../theme/constants'
+import autobind from 'autobind-decorator'
+import withState from 'recompose/withState'
 import { iconMap, searchFilter } from '../helpers'
+import { scale, breakpoints } from '../theme/constants'
 import { StyleSheet, css } from 'aphrodite/no-important'
 
 const styles = StyleSheet.create({
@@ -20,25 +24,66 @@ const styles = StyleSheet.create({
   }
 })
 
-const enhance = connect(({ query }) => ({ query }))
+@connect(({ query }) => ({ query }))
+@withState('renderedIcons', 'setRenderedIcons', [])
+export default class IconGrid extends React.Component {
 
-export default enhance(props => {
-  const icons = iconMap(props.lib)
-  return (
-    <Container>
-      <div className={css(util.flex, util.flexWrap, util.justifySpaceBetween)}>
-        {searchFilter(icons, props.query).map((icon, key) =>
-          <div
-            key={key}
-            className={css(styles.col)}>
-            <IconCard  
-              libCode={props.libCode}
-              el={icon.el}
-              name={icon.name}
-              size={30} />
-          </div>
-        )}
-      </div>
-    </Container>
-  )
-})
+  constructor (props) {
+    super(props)
+    this.chunkLength = 50
+    this.currentChunk = 0
+
+    const icons = iconMap(props.lib)
+    const filteredIcons = searchFilter(icons, props.query)
+    this.chunkedIcons = chunk(filteredIcons, this.chunkLength)
+  }
+
+  @autobind
+  scrollListener () {
+    const offset = $(window).scrollTop() + $(window).height()
+    if (offset >= $(document).height()) {
+      this.pageUp()
+    }
+  }
+
+  pageUp () {
+    this.currentChunk = this.currentChunk + 1
+    const nextChunk = this.chunkedIcons[this.currentChunk]
+    if (!nextChunk) {
+      return window.removeEventListener('scroll', this.scrollListener)
+    }
+
+    const nextRenderedIcons = [ ...this.props.renderedIcons, ...nextChunk ]
+    this.props.setRenderedIcons(nextRenderedIcons)
+  }
+
+  componentDidMount() {
+    const initialChunk = this.chunkedIcons[this.currentChunk]
+    this.props.setRenderedIcons(initialChunk)
+    window.addEventListener('scroll', this.scrollListener)
+  }
+
+  componentWillUnmount () {
+    window.removeEventListener('scroll', this.scrollListener)
+  }
+
+  render () {
+    return (
+      <Container>
+        <div className={css(util.flex, util.flexWrap, util.justifySpaceBetween)}>
+          {this.props.renderedIcons.map((icon, key) =>
+            <div
+              key={key}
+              className={css(styles.col)}>
+              <IconCard  
+                libCode={this.props.libCode}
+                el={icon.el}
+                name={icon.name}
+                size={30} />
+            </div>
+          )}
+        </div>
+      </Container>
+    )
+  }
+}
